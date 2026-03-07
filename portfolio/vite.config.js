7b4342a878b21d54986fac2a2c6433a5ebdd7097
chinmayaranjanswain <chinmayaranjanswain353@gmail.com>
2026-03-08 00:41:02 +0530

    the final commit

---
+++ portfolio/vite.config.js
@@ -0,0 +1,17 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        playground: resolve(__dirname, 'playground.html'),
+        projects: resolve(__dirname, 'projects.html'),
+        projectDetail: resolve(__dirname, 'project-detail.html'),
+      }
+    }
+  }
+});
